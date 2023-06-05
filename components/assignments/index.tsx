f4f4b6d7bf67bf476dd9f4f42d9b4b8e5884b929
Niklas Fischer <nikii17.06@gmail.com>
2023-06-05 18:54:54 +0200

    add images to assignments

--- components/assignments/index.tsx
+++ components/assignments/index.tsx
@@ -6,7 +6,6 @@ const assignments = [
     text: "Lange Zeit blieben digitale Datenmassen ungenutzt. Wie können Unternehmen Big Data im Geschäftsprozessmanagement nutzen, um daraus Chancen und Erkenntnisse zu gewinnen? Im Fokus dieser Arbeit steht zudem die Beantwortung der Frage, welche Herausforderungen und Risiken mit Big Data verbunden sind.",
     moduleId: "ANS43",
     pdfFileName: "Big_Data_und_Prozessmanagement_im_Unternehmenseinsatz",
-    image: "/assignments/cat.png",
     imageDescription: "",
     date: "14.11.2021",
   },
@@ -15,7 +14,6 @@ const assignments = [
     text: "Blockchain war zweifellos das Buzzword des letzten Jahrzehnts, aber wie sieht es tatsächlich in der Praxis aus? In dieser Arbeit werden mögliche Anwendungen untersucht, erfolgreiche Beispiele vorgestellt und potenzielle Gefahren für Nutzer und Unternehmen aufgezeigt.",
     moduleId: "DIT61",
     pdfFileName: "Blockchain_-_eine_erfolgreiche_Technologie_auch_außerhalb_des_FinTech-Sektors",
-    image: "/assignments/bird.webp",
     imageDescription: "",
     date: "01.05.2023",
   },
@@ -24,7 +22,6 @@ const assignments = [
     text: "Datenmanagement, Datensicherung und Archivierung sind essentielle Aspekte in der heutigen Informationsgesellschaft. In dieser Arbeit werden Nutzen und Aufwand dieses Prozesses diskutiert. Zudem werden die Vor- und Nachteile verschiedener Datensicherungsmedien für die Langzeitarchivierung untersucht und anhand einer fiktiven Fallstudie ein Datenmanagement- und Archivierungskonzept entwickelt.",
     moduleId: "IMG40",
     pdfFileName: "Datenmanagement",
-    image: "/assignments/racoon.webp",
     imageDescription: "",
     date: "24.03.2021",
   },
@@ -33,7 +30,6 @@ const assignments = [
     text: "Die Digitalisierung bringt einen tiefgreifenden Wandel mit sich, der nicht nur Unternehmen, sondern auch die Personalentwicklung vor neue Herausforderungen stellt. In dieser Arbeit wird einen Blick auf die wachsende Bedeutung der Personalentwicklung geworfen und die mögliche Herausforderungen reflektiert.",
     moduleId: "PER25",
     pdfFileName: "Herausforderungen_der_Personalentwicklung_im_Zeitalter_der_Digitalisierung",
-    image: "/assignments/fox.png",
     imageDescription: "",
     date: "31.12.2019",
   },
@@ -42,7 +38,6 @@ const assignments = [
     text: "In Zeiten der Digitalisierung stellen sich Unternehmen die Frage, wie sie sich profitabel und zukunftsorientiert positionieren können. Gleichzeitig rücken die Mitarbeiter als wichtigste Ressource eines Unternehmens in den Fokus. Wie lässt sich des Konzepts Ambidextrie eine Balance zwischen Stabilität und Innovation finden?",
     moduleId: "PER26",
     pdfFileName: "Personalführung_im_digitalen_Zeitalter_Merkmale_und_Bedeutung_der_Ambidextrie",
-    image: "/assignments/duck.webp",
     imageDescription: "",
     date: "01.02.2021",
   },
@@ -51,7 +46,6 @@ const assignments = [
     text: "In einer überfluteten Informationslandschaft gewinnt der bewusste Umgang mit Wissen immer mehr an Bedeutung für Unternehmen. Um im Wettbewerb zu bestehen, müssen sie ihr Wissenspotenzial systematisch erschließen und eine ganzheitliche Wissenskultur etablieren. In drei Fallstudien wird aufgezeigt, wie erfolgreiches Wissensmanagement in der Praxis umgesetzt werden kann.",
     moduleId: "PEW62",
     pdfFileName: "Wissensmanagement",
-    image: "/assignments/fish.webp",
     imageDescription: "",
     date: "26.02.2023",
   },
@@ -60,16 +54,15 @@ const assignments = [
     text: "Eine überzeugende Visualisierung von Inhalten spielt in Präsentationen eine entscheidende Rolle, um Informationen effektiv zu vermitteln. In dieser Arbeit werden die Bedeutung der Visualisierung von Inhalten untersucht und verschiedene Methoden vorstellt, um Präsentationen visuell ansprechend und wirkungsvoll zu gestalten.",
     moduleId: "SQF20",
     pdfFileName: "Visualisierung_in_einer_Praesentation",
-    image: "/assignments/deer.webp",
     imageDescription: "",
     date: "21.03.2019",
   },
   {
     title: "SWOT-Analyse",
-    text: "In einer sich ständig verändernden Geschäftswelt stehen Unternehmen vor immer neuen Herausforderungen. Die SWOT-Analyse hat sich dabei als ein wertvolles Instrument zur Strukturierung und Formulierung strategischer Entscheidungen etabliert. Wie sie zur Steigerung der Wettbewerbsfähigkeit und zur Erreichung der Unternehmensziele beiträgt, wird in dieser Arbeit anhand einer fiktiven Fallstudie erläutert.",
+    text: "In einer sich ständig verändernden Geschäftswelt stehen Unternehmen vor immer neuen Herausforderungen. Die SWOT-Analyse hat sich dabei als ein wertvolles Instrument zur Strukturierung und Formulierung strategischer Entscheidungen etabliert. Wie sie zur Steigerung der Wettbewerbsfähigkeit und zur Erreichung der Unternehmensziele beiträgt, wird anhand einer fiktiven Fallstudie erläutert.",
     moduleId: "UFU50",
     pdfFileName: "SWOT-Analyse",
-    image: "/assignments/mouse.webp",
+    image: "/assignments/UFU50/image.png",
     imageDescription: "",
     date: "15.12.2022",
   },
@@ -78,7 +71,7 @@ const assignments = [
     text: "Die Digitalisierung hat die Geschäftswelt revolutioniert und erfordert von Unternehmen eine kontinuierliche Anpassung und Innovation ihrer Geschäftsmodelle, um im Wettbewerb erfolgreich zu sein. In dieser Arbeit werden die Bedeutung der Geschäftsmodell-Innovation im Zeitalter der Digitalisierung untersucht und erfolgreiche Strategien zur Transformation von Unternehmen betrachtet.",
     moduleId: "DIT41",
     pdfFileName: "Digitalisierung_und_Geschaeftsmodell-Innovation",
-    image: "/assignments/panda.webp",
+    image: "/assignments/DIT41/image.png",
     imageDescription: "",
     date: "26.06.2022",
   },
@@ -135,7 +128,13 @@ const Card: React.FC<TCard> = ({ title, text, pdfFileName, image, imageDescripti
 
   return (
     <article className='flex flex-col overflow-hidden bg-white rounded-lg dark:bg-zinc-800'>
-      <Image src={image} alt={imageDescription} className='object-cover w-full h-64' width={800} height={256} />
+      <Image
+        src={`/assignments/${moduleId}/image.png`}
+        alt={imageDescription}
+        className='object-cover w-full h-64 bg-slate-300'
+        width={900}
+        height={256}
+      />
       <div className='flex flex-col self-stretch flex-grow p-4 border-b border-l border-r border-gray-300 rounded-b-lg dark:border-gray-900 gap-y-2'>
         <div className='flex flex-row items-center justify-between'>
           <p className='font-bold text-indigo-600 dark:text-indigo-500'>{shortDate}</p>
@@ -147,7 +146,6 @@ const Card: React.FC<TCard> = ({ title, text, pdfFileName, image, imageDescripti
           </span>
         </div>
         <h3 className='text-2xl font-semibold text-zinc-800 dark:text-zinc-100'>{title}</h3>
-
         <p className='text-zinc-800 dark:text-zinc-200'>{text}</p>
         <a
           className='flex flex-row items-center gap-1 mt-auto text-xl font-semibold text-indigo-600 hover:no-underline hover:text-indigo-700 dark:text-indigo-500 dark:hover:text-indigo-700 '
