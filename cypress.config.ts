933cbf4a0aa77233ee605b2cca55ce811f0ed6e5
Niklas Fischer <57847380+Rllyyy@users.noreply.github.com>
2023-07-29 19:06:59 +0200

    add cypress workflow
    * add workflow
    
    * integrate components tests into workflow
    
    * fix spec rgx pattern
    
    * add component + working directory to yml file
    
    * removed working directory from workflow
    
    * removed parallel
    
    * renamed cypress workflow file + switched e2e workflow tests to run build version not dev
    
    * switched "build: yarn build" to "build: yarn run build" to fix workflow build error
    
    * update build path to fix workflow build error
    
    * run cypress component tests in workflow on build
    
    * enabled cypress workflow only on main push and pull request

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "n3yjb3",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
