7097ce6b9504c2b94e9af7a33b4bc6de876a02bd
Niklas Fischer <nikii17.06@gmail.com>
2026-02-09 19:13:13 +0100

    Fix scroll navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning data-scroll-behavior='smooth'>
       <body>
         <Providers>
           <Navbar />
